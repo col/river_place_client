27bea5d624ea8ad3c44e2788bde9027b4614700b
Colin Harris <col.w.harris@gmail.com>
2016-11-12 20:44:14 +0800

    Add hex package info

--- mix.exs
+++ mix.exs
@@ -7,27 +7,25 @@ defmodule RiverPlace.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: "Library for booking tennis courts on riverplace.sg",
+     package: package,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
+  def package do
+    [
+      maintainers: ["Colin Harris"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/col/river_place"}
+    ]
+  end
+
   def application do
     [
       applications: [:logger, :httpoison]
     ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [{:httpoison, "~> 0.10.0"},
      {:poison, "~> 3.0"}]
